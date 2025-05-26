951fcd10ce4bad6e5ded1bda945c52a1359c859a
Oscar <oscarrpeulicke@gmail.com>
2025-05-26 22:01:11 +0200

    pickRandom

--- src/random.spec.ts
+++ src/random.spec.ts
@@ -1,4 +1,17 @@
-import { shuffle } from "./random.js";
+import { pickRandom, randomInt, shuffle } from "./random.js";
+
+it("gets a random int", () => {
+    expect(randomInt(() => 0)(0, 10)).toBe(0);
+    expect(randomInt(() => 0.99)(0, 10)).toBe(10);
+});
+
+it("picks a random element", () => {
+    const array = [1, 2, 3, 4];
+    expect(pickRandom(() => 0.1)(array)).toBe(1);
+    expect(pickRandom(() => 0.4)(array)).toBe(2);
+    expect(pickRandom(() => 0.6)(array)).toBe(3);
+    expect(pickRandom(() => 0.99)(array)).toBe(4);
+});
 
 it("shuffles", () => {
     const array = [1, 2, 3, 4];
--- src/random.ts
+++ src/random.ts
@@ -3,6 +3,11 @@ import { swap } from "./basic.js";
 export const randomInt = (random: () => number) => (min: number, max: number) =>
     Math.floor(random() * (max - min + 1)) + min;
 
+export const pickRandom =
+    (random: () => number) =>
+    <T>(objects: T[]) =>
+        objects[randomInt(random)(0, objects.length - 1)];
+
 export const shuffle = <T>(objects: T[], random: () => number) => {
     for (let i = 0; i + 1 < objects.length; ++i) {
         swap(objects, i, randomInt(random)(i + 1, objects.length - 1));
